d71a81507f06200bc62f8ceb05742d4e727782ee
Eli James <ejames@lead.bank>
2025-10-10 09:38:07 -0500

    edits

--- variables.tf
+++ variables.tf
@@ -11,3 +11,13 @@ variable "lambda_api_key" {
   type      = string
   sensitive = true
 }
+
+variable "lambda_artifact_bucket" {
+  type    = string
+  default = "my-lambda-artifacts-lead"
+}
+
+variable "lambda_s3_key" {
+  type = string
+  description = "S3 key for the lambda artifact, set by CI (e.g. onboarding/lambda-<sha>.zip)"
+}
